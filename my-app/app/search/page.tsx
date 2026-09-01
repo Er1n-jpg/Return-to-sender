43d85e963e2dbeb140b30d631c7b027a66bdcd4b
Erin <enguy4@ocdsb.ca>
2026-09-01 16:51:01 -0400

    the build works now on terminal

--- my-app/app/search/page.tsx
+++ my-app/app/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { supabase } from 'lib/supabase'
+import { supabase } from '@/lib/supabase'
 import type { Session } from '@supabase/supabase-js'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
